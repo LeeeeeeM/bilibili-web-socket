5e78678f0f0d35e263407f3756b0d52c49868a1c
evilemon <slm_1990@126.com>
2017-12-27 22:42:46 +0800

    init repo

---
+++ module.js
@@ -0,0 +1,176 @@
+var dataStruct = [{
+    name: "Header Length",
+    key: "headerLen",
+    bytes: 2,
+    offset: 4,
+    value: 16
+}, {
+    name: "Protocol Version",
+    key: "ver",
+    bytes: 2,
+    offset: 6,
+    value: 1
+}, {
+    name: "Operation",
+    key: "op",
+    bytes: 4,
+    offset: 8,
+    value: 1
+}, {
+    name: "Sequence Id",
+    key: "seq",
+    bytes: 4,
+    offset: 12,
+    value: 1
+}]
+
+var socket = new WebSocket('ws://broadcastlv.chat.bilibili.com:2244/sub')
+socket.binaryType = 'arraybuffer';
+
+socket.onopen = function (event) {
+    var join = joinRoom();
+    socket.send(join.buffer);
+    sendBeat();
+}
+
+socket.onmessage = function (event) {
+    var dataView = new DataView(event.data);
+    var data = {};
+    data.packetLen = dataView.getUint32(0);
+    dataStruct.forEach(function (item) {
+        if (item.bytes === 4) {
+            data[item.key] = dataView.getUint32(item.offset)
+        } else if (item.bytes === 2) {
+            data[item.key] = dataView.getUint16(item.offset)
+        }
+    });
+    if (data.op && data.op === 5) {
+        data.body = [];
+        var packetLen = data.packetLen
+        for (var offset = 0; offset < dataView.byteLength; offset += packetLen) {
+            packetLen = dataView.getUint32(offset)
+            headerLen = dataView.getUint16(offset + 4)
+
+            var recData = [];
+            for (var i = headerLen; i < packetLen; i++) {
+                recData.push(dataView.getUint8(i))
+            }
+            try {
+                // console.log(bytes2str(recData))
+                let body = JSON.parse(bytes2str(recData))
+                if (body.cmd === 'DANMU_MSG') {
+                    console.log(body.info[2][1], ':', body.info[1])
+                }
+                data.body.push(body)
+            } catch (e) {
+                // console.log('tcp 校验失败，重新发送');
+            }
+        }
+    }
+
+}
+
+function sendBeat() {
+    var timer = setInterval(function () {
+        socket.send(generatePacket())
+    }, 3000);
+}
+
+
+function str2bytes(str) {
+    var bytes = new Array();
+    var len, c;
+    len = str.length;
+    for (var i = 0; i < len; i++) {
+        c = str.charCodeAt(i);
+        if (c >= 0x010000 && c <= 0x10FFFF) {
+            bytes.push(((c >> 18) & 0x07) | 0xF0);
+            bytes.push(((c >> 12) & 0x3F) | 0x80);
+            bytes.push(((c >> 6) & 0x3F) | 0x80);
+            bytes.push((c & 0x3F) | 0x80);
+        } else if (c >= 0x000800 && c <= 0x00FFFF) {
+            bytes.push(((c >> 12) & 0x0F) | 0xE0);
+            bytes.push(((c >> 6) & 0x3F) | 0x80);
+            bytes.push((c & 0x3F) | 0x80);
+        } else if (c >= 0x000080 && c <= 0x0007FF) {
+            bytes.push(((c >> 6) & 0x1F) | 0xC0);
+            bytes.push((c & 0x3F) | 0x80);
+        } else {
+            bytes.push(c & 0xFF);
+        }
+    }
+    return bytes;
+}
+
+
+function bytes2str(array) {
+    var __array = array.slice(0);
+    var j;
+    var filterArray = [
+        [0x7f],
+        [0x1f, 0x3f],
+        [0x0f, 0x3f, 0x3f],
+        [0x07, 0x3f, 0x3f, 0x3f]
+    ];
+    var str = '';
+    for (var i = 0; i < __array.length; i = i + j) {
+        var item = __array[i];
+        var number = '';
+        if (item >= 240) {
+            j = 4;
+        } else if (item >= 224) {
+            j = 3;
+        } else if (item >= 192) {
+            j = 2;
+        } else if (item < 128) {
+            j = 1;
+        }
+        var filter = filterArray[j - 1];
+        for (var k = 0; k < j; k++) {
+            var r = (__array[i + k] & filter[k]).toString(2);
+            var l = r.length;
+            if (l > 6) {
+                number = r;
+                break;
+            }
+            for (var n = 0; n < 6 - l; n++) {
+                r = '0' + r
+            }
+            number = number + r;
+        }
+        str = str + String.fromCharCode(parseInt(number, 2));
+    }
+    return str;
+}
+
+
+var joinRoom = function (rid, uid) {
+    rid = rid || 282712;
+    uid = uid || 19176530;
+    var packet = JSON.stringify({
+        uid: uid,
+        roomid: rid
+    });
+    return generatePacket(7, packet);
+}
+
+function getPacket(payload) {
+    return str2bytes(payload);
+}
+
+function generatePacket(action, payload) {
+    action = action || 2 // 2心跳  或  7加入房间
+    payload = payload || ''
+    var packet = getPacket(payload)
+    var buff = new ArrayBuffer(packet.length + 16);
+    var dataBuf = new DataView(buff);
+    dataBuf.setUint32(0, packet.length + 16);
+    dataBuf.setUint16(4, 16);
+    dataBuf.setUint16(6, 1);
+    dataBuf.setUint32(8, action);
+    dataBuf.setUint32(12, 1);
+    for (var i = 0; i < packet.length; i++) {
+        dataBuf.setUint8(16 + i, packet[i]);
+    }
+    return dataBuf;
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+# 目前只是web通过websocket链接
+
+**之后还会带来server通过tcp和websocket协议链接**
+
+**web版不同于server版本（需要通过Buffer来传输二进制数据），web版本必须走ArrayBuffer**
